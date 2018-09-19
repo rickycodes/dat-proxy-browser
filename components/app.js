aaf77ceaaa92bbd2d3dade005adadc6176b62a52
rickycodes <ricky.miller@gmail.com>
2018-09-19 02:13:22 -0400

    Add OmniSearch

--- components/app.js
+++ components/app.js
@@ -6,22 +6,12 @@ import {
   View
 } from 'react-native';
 
-// these need to go elsewhere
 import styles from '../styles'
-import { PROXY_DOMAIN } from '../constants'
+import OmniSearch from './omniSearch';
 
 export default class App extends Component {
-  onSubmitEditing = _ => {
-    this.props.cleanSearchUrl(this.props.input)
-  }
-
   onNavigationStateChange = state => {
     this.props.setDisplayUrl(state.url)
-  }
-
-  getValue = url => {
-    const regEx = new RegExp(PROXY_DOMAIN, 'i')
-    return regEx.test(url) ? this.props.input : url
   }
 
   render() {
@@ -29,16 +19,7 @@ export default class App extends Component {
 
     return (
       <View style={styles.container}>
-        <View style={styles.toolbar}>
-          <TextInput
-            autoCapitalize='none'
-            autoCorrect={false}
-            style={styles.input}
-            onChangeText={this.props.setInput}
-            onSubmitEditing={this.onSubmitEditing}
-            value={this.getValue(display_url)}
-          />
-        </View>
+        <OmniSearch {...this.props} />
         {url && !loading && <WebView
           ref={r => this.webview = r}
           source={{ uri: url }}
---
+++ components/omniSearch.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+import { PROXY_DOMAIN } from '../constants'
+import styles from '../styles'
+
+class OmniSearch extends Component {
+  getValue = url => {
+    const regEx = new RegExp(PROXY_DOMAIN, 'i')
+    return regEx.test(url) ? this.props.input : url
+  }
+
+  onSubmitEditing = _ => {
+    this.props.cleanSearchUrl(this.props.input)
+  }
+
+  render() {
+    const { display_url } = this.props
+
+    return (
+      <View style={styles.toolbar}>
+        <TextInput
+          autoCapitalize='none'
+          autoCorrect={false}
+          style={styles.input}
+          onChangeText={this.props.setInput}
+          onSubmitEditing={this.onSubmitEditing}
+          value={this.getValue(display_url)}
+        />
+      </View>
+    )
+  }
+}
+
+export default OmniSearch
